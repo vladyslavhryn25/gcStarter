6ad40020f525c2916551d1ac95ee2e4a68682fd6
Vladyslav Hryn <vladyslav.hryn.25@kse.og.ua>
2026-03-28 21:59:08 +0200

    Stage 1 — Data class

--- Task2/obj/Debug/net10.0/Task2.AssemblyInfo.cs
+++ Task2/obj/Debug/net10.0/Task2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3068116518dde28a12773ca7d55d49384497bd82")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0b94bb569398fe2f63407e6e53d7f8f3ea239ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Task3/DataItem.cs
@@ -0,0 +1,12 @@
+﻿namespace Task3;
+
+public class DataItem
+{
+    public string Id { get; set; } = string.Empty;
+    public byte[] Payload { get; set; } = Array.Empty<byte>();
+
+    public long GetSize()
+    {
+        return Payload.Length;
+    }
+}
