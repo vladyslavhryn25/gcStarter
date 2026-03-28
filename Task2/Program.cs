f0b94bb569398fe2f63407e6e53d7f8f3ea239ec
Vladyslav Hryn <vladyslav.hryn.25@kse.og.ua>
2026-03-28 21:50:25 +0200

    Stage 3 — Analysis

---
+++ Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿using Task2;
+
+Point p1 = new Point { X = 1, Y = 2 };
+Point p2 = p1;
+p2.X = 10;
+p1.Print();
+p2.Print(); 
+
+Console.WriteLine();
+
+PointRef r1 = new PointRef { X = 1, Y = 2 };
+PointRef r2 = r1;
+r2.X = 10; 
+r1.Print();
+r2.Print(); 
+
+Console.WriteLine();
+
+
+BoxingTester.Test();
