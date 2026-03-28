575eb9ca75c7ca811a19d7213ee7043a714d5a22
Vladyslav Hryn <vladyslav.hryn.25@kse.og.ua>
2026-03-28 22:06:13 +0200

    Stage 3 — GC

---
+++ Task3/Program.cs
@@ -0,0 +1,21 @@
+﻿using Task3;
+
+var container = new DataContainer<DataItem>();
+
+for (int i = 0; i < 10000; i++)
+{
+    var item = new DataItem
+    {
+        Id = $"item-{i}",
+        Payload = new byte[1024]
+    };
+    container.AddItem(item);
+}
+
+Console.WriteLine($"Total size before GC: {container.GetTotalSize()} bytes");
+
+
+GC.Collect();
+
+Console.WriteLine("GC.Collect() was called.");
+Console.WriteLine($"Total size after GC: {container.GetTotalSize()} bytes");
