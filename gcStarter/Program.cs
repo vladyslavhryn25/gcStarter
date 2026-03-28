3068116518dde28a12773ca7d55d49384497bd82
Vladyslav Hryn <vladyslav.hryn.25@kse.og.ua>
2026-03-28 21:11:53 +0200

    Stage 3

--- gcStarter/Program.cs
+++ gcStarter/Program.cs
@@ -1,1 +1,19 @@
-﻿    
+﻿using gcStarter;
+var file = new FileResource("report.txt");
+var network = new NetworkResource("api.company.local");
+
+var manager = new ResourceManager<Resource>();
+
+manager.Add(file);
+manager.Add(network);
+manager.OpenAll();
+
+
+using (var tempFile = new FileResource("temp.txt"))
+{
+    tempFile.Open();
+}
+
+manager.CloseAll();
+
+Console.WriteLine("Done");
