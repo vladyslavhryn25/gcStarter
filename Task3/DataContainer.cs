83b0c33a6292ee8800877d8518cb0a71f8dbcca0
Vladyslav Hryn <vladyslav.hryn.25@kse.og.ua>
2026-03-28 22:02:37 +0200

    Stage 2 — Generic Container

---
+++ Task3/DataContainer.cs
@@ -0,0 +1,19 @@
+﻿namespace Task3;
+
+public class DataContainer<T> where T : DataItem
+{
+    private readonly List<T> _items = new();
+
+    public void AddItem(T item)
+    {
+        _items.Add(item);
+    }
+
+    public long GetTotalSize()
+    {
+        long total = 0;
+        foreach (var item in _items)
+            total += item.GetSize();
+        return total;
+    }
+}
