43ffe362fd6f6538dfa01bb3c75751407aba39d6
Vladyslav Hryn <vladyslav.hryn.25@kse.og.ua>
2026-03-28 21:45:22 +0200

    Stage 2 — Boxing

---
+++ Task2/BoxingTester.cs
@@ -0,0 +1,17 @@
+﻿namespace Task2;
+
+public static class BoxingTester
+{
+    public static void Test()
+    {
+        int a = 10;
+
+        object b = a;
+        
+        int c = (int)b;
+
+        Console.WriteLine($"a = {a}");
+        Console.WriteLine($"b (boxed) = {b}");
+        Console.WriteLine($"c (unboxed) = {c}");
+    }
+}
