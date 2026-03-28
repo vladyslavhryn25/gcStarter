4e8488dfa1acd444a9ac8378e49c1f78b3324dcb
Vladyslav Hryn <vladyslav.hryn.25@kse.og.ua>
2026-03-28 21:07:15 +0200

    Stage 2

---
+++ gcStarter/ResourceManager.cs
@@ -0,0 +1,24 @@
+﻿namespace gcStarter;
+using System.Collections.Generic;
+
+public class ResourceManager<T> where T : Resource
+{
+    private readonly List<T> _resources = new();
+
+    public void Add(T resource)
+    {
+        _resources.Add(resource);
+    }
+
+    public void OpenAll()
+    {
+        foreach (var r in _resources)
+            r.Open();
+    }
+
+    public void CloseAll()
+    {
+        foreach (var r in _resources)
+            r.Close();
+    }
+}
